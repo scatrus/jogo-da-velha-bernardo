aa40fb8041ae73fa433e30b1c7b24c1d0156602c
scatrus <12qwasZXwikb6nms>
2022-08-28 21:00:09 -0300

    Revert "Revert "inicial""
    This reverts commit f7cb885c9d92f6114b7c1824f7c210ab9ec55d7f.

---
+++ lib/main.dart
@@ -0,0 +1,185 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'Utils.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  static const String title = 'Jogo da Velha';
+
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: title,
+        theme: ThemeData(
+          primaryColor: Colors.black,
+        ),
+        home: const MainPage(title: title),
+      );
+}
+
+class MainPage extends StatefulWidget {
+  final String title;
+
+  const MainPage({
+    required this.title,
+  });
+
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class Player {
+  static const none = '';
+  static const X = 'X';
+  static const O = 'O';
+}
+
+class _MainPageState extends State<MainPage> {
+  static const countMatrix = 3;
+  static const double size = 92;
+
+  String lastMove = Player.none;
+  late List<List<String>> matrix;
+
+  @override
+  void initState() {
+    super.initState();
+
+    setEmptyFields();
+  }
+
+  void setEmptyFields() => setState(() => matrix = List.generate(
+        countMatrix,
+        (_) => List.generate(countMatrix, (_) => Player.none),
+      ));
+
+  Color getBackgroundColor() {
+    final thisMove = lastMove == Player.X ? Player.O : Player.X;
+
+    return getFieldColor(thisMove).withAlpha(150);
+  }
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        backgroundColor: getBackgroundColor(),
+        appBar: AppBar(
+          title: Center(child: Text(widget.title)),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: Utils.modelBuilder(matrix, (x, value) => buildRow(x)),
+        ),
+        bottomNavigationBar: IconButton(
+          color: Colors.white,
+          iconSize: 50,
+          onPressed: setEmptyFields,
+          icon: const Icon(Icons.restart_alt),
+        ),
+      );
+  //teste
+  Widget buildRow(int x) {
+    final values = matrix[x];
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: Utils.modelBuilder(
+        values,
+        (y, value) => buildField(x, y),
+      ),
+    );
+  }
+
+  Color getFieldColor(String value) {
+    switch (value) {
+      case Player.O:
+        return Colors.blue;
+      case Player.X:
+        return Colors.red;
+      default:
+        return Colors.white;
+    }
+  }
+
+  Widget buildField(int x, int y) {
+    final value = matrix[x][y];
+    final color = getFieldColor(value);
+
+    return Container(
+      margin: const EdgeInsets.all(4),
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          minimumSize: const Size(size, size),
+          primary: color,
+        ),
+        child: Text(value, style: const TextStyle(fontSize: 32)),
+        onPressed: () => selectField(value, x, y),
+      ),
+    );
+  }
+
+  void selectField(String value, int x, int y) {
+    if (value == Player.none) {
+      final newValue = lastMove == Player.X ? Player.O : Player.X;
+
+      setState(() {
+        lastMove = newValue;
+        matrix[x][y] = newValue;
+      });
+
+      if (isWinner(x, y)) {
+        showEndDialog('Jogador $newValue Venceu!!');
+      } else if (isEnd()) {
+        showEndDialog('Deu Velha!!');
+      }
+    }
+  }
+
+  bool isEnd() =>
+      matrix.every((values) => values.every((value) => value != Player.none));
+
+  /// Check out logic here: https://stackoverflow.com/a/1058804
+  bool isWinner(int x, int y) {
+    var col = 0, row = 0, diag = 0, rdiag = 0;
+    final player = matrix[x][y];
+    final n = countMatrix;
+
+    for (int i = 0; i < n; i++) {
+      if (matrix[x][i] == player) col++;
+      if (matrix[i][y] == player) row++;
+      if (matrix[i][i] == player) diag++;
+      if (matrix[i][n - i - 1] == player) rdiag++;
+    }
+
+    return row == n || col == n || diag == n || rdiag == n;
+  }
+
+  Future showEndDialog(String title) => showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (context) => AlertDialog(
+          title: Text(title),
+          content: const Text('Aperte para recomeçar'),
+          actions: [
+            ElevatedButton(
+              onPressed: () {
+                setEmptyFields();
+                Navigator.of(context).pop();
+              },
+              child: const Text('Reiniciar'),
+            )
+          ],
+        ),
+      );
+}
